07ff7169c4636c9c38368cf65fe12a4fdcf72f37
Doohl <dhlserrano@gmail.com>
2020-06-07 17:54:51 -0700

    Some weirdness w/ private key

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const admin = require('firebase-admin');
 // });
 admin.initializeApp({
 	credentials: admin.credential.cert({
-		"private_key": process.env.FIREBASE_PRIVATE_KEY,
+		"private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 		"client_email": process.env.FIREBASE_CLIENT_EMAIL,
 		"project_id": process.env.FIREBASE_PROJECT_ID,
 		"client_id": process.env.FIREBASE_CLIENT_ID,
